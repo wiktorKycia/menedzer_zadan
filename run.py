728904ef54b4032bb8745b88f2c24e157996b4d5
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-02-01 18:36:41 +0100

    stworzenie funkcji dodawania zadania

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ root = Tk()
 root.geometry("500x500")
 frames_list = []
 
+
 def import_from_file(file_name):
     file = open(file_name, "r")
     global list_of_tasks
@@ -14,10 +15,10 @@ def import_from_file(file_name):
 def show_tasks():
     import_from_file("tasks.txt")
     global frame
-    frames_list.append(frame)
     for i, task in enumerate(list_of_tasks):
         frame = Frame(root, height=100, relief=RAISED, borderwidth=5)
         frame.pack(fill=X)
+        frames_list.append(frame)
 
         global label1
         label1 = Label(frame, text=str(i+1)+": ", padx=10)
@@ -32,8 +33,20 @@ def show_tasks():
         delete_button.pack(side=RIGHT)
 
 
+def add_task():
+    file = open("tasks.txt", "a+")
+    new_task = str(task_name)
+    file.write(new_task+"\n")
+    file.close()
+    show_tasks()
+
+
 show_tasks()
 
-plus = Button(root, text="+")
+
+task_name = StringVar()
+entry = Entry(root, textvariable=task_name).pack()
+plus = Button(root, text="+", command=add_task)
+plus.pack(after=entry)
 
 root.mainloop()
