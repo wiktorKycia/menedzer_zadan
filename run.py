c24e24dafb56ca91bad06cf11b71157c10417858
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-02-01 18:18:10 +0100

    dodanie przycisku plus, listy do framów

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.geometry("500x500")
-
+frames_list = []
 
 def import_from_file(file_name):
     file = open(file_name, "r")
@@ -14,6 +14,7 @@ def import_from_file(file_name):
 def show_tasks():
     import_from_file("tasks.txt")
     global frame
+    frames_list.append(frame)
     for i, task in enumerate(list_of_tasks):
         frame = Frame(root, height=100, relief=RAISED, borderwidth=5)
         frame.pack(fill=X)
@@ -23,12 +24,16 @@ def show_tasks():
         label1.pack(side=LEFT)
 
         global label2
-        label2 = Label(frame, text=list_of_tasks[i])
+        label2 = Label(frame, text=task)
         label2.pack()
 
         global delete_button
         delete_button = Button(frame, text="delete")
         delete_button.pack(side=RIGHT)
+
+
 show_tasks()
+
+plus = Button(root, text="+")
 
 root.mainloop()
