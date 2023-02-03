ce86f1e248fcdc83bde0acf8a5f43b72edfcf04f
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-02-03 11:47:27 +0100

    utowrzenie labela i okienka do wpisywania

--- run2.py
+++ run2.py
@@ -1,1 +1,27 @@
 from tkinter import *
+
+root = Tk()
+root.geometry("500x200")
+
+label_var = StringVar(root, 'Find in the list or type here: ')
+label = Label(root, textvariable=label_var)
+label.pack()
+label.place(x=10, y=10)
+
+entry_var = StringVar(root)
+entry_var.set('type task name')
+edit = Entry(root, textvariable=entry_var)
+edit.pack()
+edit.place(x=10, y=30, width=150)
+
+
+def import_from_file(file_name):
+    file = open(file_name, "r")
+    global list_of_tasks
+    list_of_tasks = file.readlines()
+    file.close()
+
+
+import_from_file('tasks.txt')
+
+root.mainloop()
