fff746526ae2a50363620fb8377e2b92925dd3cf
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-02-04 15:00:20 +0100

    utworzenie funkcji na dodawanie i usuwanie tasków

--- run2.py
+++ run2.py
@@ -1,8 +1,11 @@
 from tkinter import *
+import os
 
 root = Tk()
 root.geometry("500x200")
 
+# WIDGETS
+# Entry
 label_var = StringVar(root, 'Find in the list or type here: ')
 label = Label(root, textvariable=label_var)
 label.pack()
@@ -14,6 +17,7 @@ edit = Entry(root, textvariable=entry_var)
 edit.pack()
 edit.place(x=10, y=30, width=150)
 
+# Listbox
 list_label = Label(root, text='Here is your list of tasks')
 list_label.pack()
 list_label.place(x=10, y=60)
@@ -23,6 +27,7 @@ listbox.pack()
 listbox.place(x=10, y=80)
 
 
+# FUNCTIONS
 def import_from_file(file_name):
     file = open(file_name, "r")
     global list_of_tasks
@@ -35,8 +40,35 @@ def show_tasks():
     for task in list_of_tasks:
         listbox.insert(END, task)
 
-# zrobić funkcje pokaż, która wypisuje zadania z list_od_tasks w listboxie
-# buttony: edit_task, add_task, remove_task
+
+def remove_task():
+    import_from_file('tasks.txt')
+    task_name = str(entry_var)
+    for task in list_of_tasks:
+        if task == task_name:
+            list_of_tasks.remove(task)
+    os.remove('tasks.txt')
+    file = open('tasks.txt', 'a+')
+    for task in list_of_tasks:
+        file.write(task)
+    show_tasks()
+
+
+def add_task():
+    import_from_file('tasks.txt')
+    new_task = str(entry_var)
+    list_of_tasks.append(new_task)
+    file = open('tasks.txt', 'a+')
+    file.write(new_task+"\n")
+    file.close()
+    show_tasks()
+'''
+def clear_tasks():
+    file = open('tasks.txt', 'a+')
+    file.
+'''
+
+# buttony: edit_task, 
 
 show_tasks()
 
